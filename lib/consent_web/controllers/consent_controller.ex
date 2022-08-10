265e3e25aa72cd2bb4281cecfa57950033867165
Peter Zingg <peter.zingg@gmail.com>
2022-08-10 00:15:27 -0700

    alpinejs version - much lighter

--- lib/consent_web/controllers/consent_controller.ex
+++ lib/consent_web/controllers/consent_controller.ex
@@ -1,7 +1,10 @@
 defmodule ConsentWeb.ConsentController do
   use ConsentWeb, :controller
 
+  alias Consent.Accounts.ConsentGroup
   alias ConsentWeb.{ConsentHelpers, UserAuth}
+
+  @current_terms_version "1.1.0"
 
   # We come here to update the cookie_consent cookie
   def update(conn, %{"consent_params" => consent_params}) do
@@ -25,6 +28,25 @@ defmodule ConsentWeb.ConsentController do
   end
 
   def alpine_modal(conn, _params) do
-    render(conn, "alpine_modal.html")
+    cookie_consent = get_session(conn, :cookie_consent)
+    consented_groups = cookie_consent.groups
+
+    groups_with_index =
+      ConsentGroup.builtin_groups()
+      |> Enum.map(fn {_slug, group} ->
+        ConsentGroup.set_consent(group, consented_groups)
+        |> Map.from_struct()
+      end)
+      |> Enum.with_index()
+
+    form_action = Routes.consent_update_path(conn, :update)
+    csrf_token = Phoenix.HTML.Tag.csrf_token_value(form_action)
+
+    render(conn, "alpine_modal.html",
+      form_action: form_action,
+      csrf_token: csrf_token,
+      terms_version: @current_terms_version,
+      groups_with_index: groups_with_index
+    )
   end
 end
