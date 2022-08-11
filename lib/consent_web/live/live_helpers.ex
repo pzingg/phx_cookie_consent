caa788a04efb2c53c40dd131f48ab3d947a92f35
Peter Zingg <peter.zingg@gmail.com>
2022-08-10 21:49:21 -0700

    consent_modal component

--- lib/consent_web/live/live_helpers.ex
+++ lib/consent_web/live/live_helpers.ex
@@ -138,79 +138,75 @@ defmodule ConsentWeb.LiveHelpers do
             <!-- had phx-window-keydown on this next element -->
             <div id={"#{@layout_id}-container"} class="sticky inline-block px-4 pt-5 pb-4 overflow-hidden text-left align-bottom transform bg-white rounded-lg shadow-xl fade-in-scale sm:my-8 sm:align-middle sm:max-w-xl sm:w-full sm:p-6">
               <!-- a id="modal-return-to" class="hidden" href="/"></a -->
-              <div class="sm:flex sm:items-start">
-                <div class="flex items-center justify-center flex-shrink-0 w-8 h-8 mx-auto bg-purple-100 rounded-full sm:mx-0">
-                  <!-- Heroicon name: outline/plus -->
-                  <%= @icon %>
-                </div>
-                <div class="w-full mt-3 mr-12 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                  <h3 class="text-lg font-medium leading-6 text-gray-900" id={"#{@layout_id}-title"}>
-                    <%= @title %>
-                  </h3>
-                  <div class="mt-2">
-                    <p id={"#{@layout_id}-content"} class="text-sm text-gray-500">
-                      <div id={"#{@form_id}-container"}>
-                        <form action={@form_action} method="post" id={@form_id}>
-                          <input name="_csrf_token" type="hidden" value={@csrf_token}>
+              <div class="w-full mt-3 text-center sm:mt-0 sm:text-left">
+                <h3 class="text-lg font-medium leading-6 text-gray-900" id={"#{@layout_id}-title"}>
+                  <%= @title %>
+                </h3>
+                <div class="mt-2">
+                  <p id={"#{@layout_id}-content"} class="text-sm text-gray-500">
+                    <div id={"#{@form_id}-container"}>
+                      <form action={@form_action} method="post" id={@form_id}>
+                        <input name="_csrf_token" type="hidden" value={@csrf_token}>
 
-                          <div class="pt-2 pb-2 border-t border-gray-200">
-                            <h3 class="text-lg font-medium leading-6 text-gray-900">This site uses cookies</h3>
-                            <div class="pb-2 mt-4 text-sm">
-                              We use cookies on this site so we can provide you with personalised content,
-                              ads and to analyze our website's traffic.
-                              Check the boxes below to agree to our terms and conditions and
-                              to allow or restrict the cookies being used.
+                        <div class="pt-2 pb-2 border-t border-gray-200">
+                          <h3 class="text-lg font-medium leading-6 text-gray-900">This site uses cookies</h3>
+                          <div class="pb-2 mt-4 text-sm">
+                            We use cookies on this site so we can provide you with personalised content,
+                            ads and to analyze our website's traffic.
+                            Check the boxes below to agree to our terms and conditions and
+                            to allow or restrict the cookies being used.
+                          </div>
+                        </div>
+
+                        <div class="pt-2 pb-2 border-t border-gray-200">
+                          <div class="sm:flex sm:items-center sm:justify-between">
+                            <h3 class="text-lg font-medium leading-6 text-gray-900">Terms and conditions</h3>
+                            <div class="mt-3 sm:mt-0 sm:ml-4">
+                              <input name={"#{@as}[terms_version]"} type="hidden" id={"#{@form_id}_terms_version"} value="1.1.0">
+                              <label class="block mb-1 text-gray-700">
+                                <span class="mr-2 text-red-600 ">Required</span>
+                                <input name={"#{@as}[terms_agreed]"} type="hidden" value="false">
+                                <input name={"#{@as}[terms_agreed]"} type="checkbox" checked required id={"#{@form_id}_terms_agreed"} value="true">
+                              </label>
                             </div>
                           </div>
+                          <div class="mt-4 text-sm">
+                            Please read the site's Terms and Conditions, version <%= @terms_version %>.
+                            By continuing to use this website, you consent to those terms.
+                          </div>
+                        </div>
 
-                          <div class="pt-2 pb-2 border-t border-gray-200">
-                            <div class="sm:flex sm:items-center sm:justify-between">
-                              <h3 class="text-lg font-medium leading-6 text-gray-900">Terms and conditions</h3>
-                              <div class="mt-3 sm:mt-0 sm:ml-4">
-                                <input name={"#{@as}[terms_version]"} type="hidden" id={"#{@form_id}_terms_version"} value="1.1.0">
-                                <label class="block mb-1 text-gray-700">
-                                  <span class="mr-2 text-red-600 ">Required</span>
-                                  <input name={"#{@as}[terms_agreed]"} type="hidden" value="false">
-                                  <input name={"#{@as}[terms_agreed]"} type="checkbox" checked required id={"#{@form_id}_terms_agreed"} value="true">
-                                </label>
-                              </div>
+                        <%= for {cg, i} <- @groups_with_index do %>
+                        <div class="pt-2 pb-2 border-t border-gray-200">
+                          <div class="sm:flex sm:items-center sm:justify-between">
+                            <div class="flex-initial w-96">
+                              <h3 @click={"showGroup = showGroup == '#{cg.slug}' ? '' : '#{cg.slug}'"} class="py-1 pl-2 pr-6 text-lg font-medium leading-6 text-gray-900 border border-blue-300 rounded"><%= cg.title %></h3>
                             </div>
-                            <div class="mt-4 text-sm">
-                              Please read the site's Terms and Conditions, version <%= @terms_version %>.
-                              By continuing to use this website, you consent to those terms.
+                            <div class="mt-3 sm:mt-0 sm:ml-4">
+                              <input name={"#{@as}[groups][#{i}][slug]"} type="hidden" id={"#{@form_id}_#{cg.slug}_slug"} value={cg.slug}>
+                              <label class="block mb-1">
+                                <%= if cg.required do %>
+                                <span class="mr-2 text-red-600">Required</span>
+                                <input name={"#{@as}    </div>
+                                [groups][#{i}][consent_given]"} type="hidden" value="false">
+                                <input name={"#{@as}[groups][#{i}][consent_given]"} type="checkbox" checked required id={"#{@form_id}_#{cg.slug}_consent_given"} value="true">
+                                <% else %>
+                                <span class="mr-2">Allow?</span>
+                                <input name={"#{@as}[groups][#{i}][consent_given]"} type="hidden" value="false">
+                                <input name={"#{@as}[groups][#{i}][consent_given]"} type="checkbox" checked={cg.consent_given} id={"#{@form_id}_#{cg.slug}_consent_given"} value="true">
+                                <% end %>
+                              </label>
                             </div>
                           </div>
-
-                          <%= for {cg, i} <- @groups_with_index do %>
-                          <div class="pt-2 pb-2 border-t border-gray-200">
-                            <div class="sm:flex sm:items-center sm:justify-between">
-                              <h3 @click={"showGroup = showGroup == '#{cg.slug}' ? '' : '#{cg.slug}'"} class="px-6 py-2 text-lg font-medium leading-6 text-gray-900 border border-blue-300 rounded"><%= cg.title %></h3>
-                              <div class="mt-3 sm:mt-0 sm:ml-4">
-                                <input name={"#{@as}[groups][#{i}][slug]"} type="hidden" id={"#{@form_id}_#{cg.slug}_slug"} value={cg.slug}>
-                                <label class="block mb-1">
-                                  <%= if cg.required do %>
-                                  <span class="mr-2 text-red-600">Required</span>
-                                  <input name={"#{@as}    </div>
-                                  [groups][#{i}][consent_given]"} type="hidden" value="false">
-                                  <input name={"#{@as}[groups][#{i}][consent_given]"} type="checkbox" checked required id={"#{@form_id}_#{cg.slug}_consent_given"} value="true">
-                                  <% else %>
-                                  <span class="mr-2">Allow?</span>
-                                  <input name={"#{@as}[groups][#{i}][consent_given]"} type="hidden" value="false">
-                                  <input name={"#{@as}[groups][#{i}][consent_given]"} type="checkbox" checked={cg.consent_given} id={"#{@form_id}_#{cg.slug}_consent_given"} value="true">
-                                  <% end %>
-                                </label>
-                              </div>
-                            </div>
-                            <div x-show={"showGroup == '#{cg.slug}'"} id={"#{@form_id}_#{cg.slug}_description"} class="mt-4 text-sm">
-                              <%= cg.description %>
-                            </div>
+                          <div x-show={"showGroup == '#{cg.slug}'"} id={"#{@form_id}_#{cg.slug}_description"} class="mt-4 text-sm">
+                            <%= cg.description %>
                           </div>
-                          <% end %>
+                        </div>
+                        <% end %>
 
-                        </form>
-                      </div>
-                    </p>
-                  </div>
+                      </form>
+                    </div>
+                  </p>
                 </div>
               </div>
               <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
