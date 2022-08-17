9fd4caa98e0fb73aeb725bbc7b2b298be0248549
Peter Zingg <peter.zingg@gmail.com>
2022-08-17 16:57:06 -0700

    typo

--- config/runtime.exs
+++ config/runtime.exs
@@ -47,7 +47,7 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: String.to_integer(System.get_env("PORT") || "4000")
     ],
-    secret_key_base: secret_key_basese
+    secret_key_base: secret_key_base
 
   # ## Using releases
   #
